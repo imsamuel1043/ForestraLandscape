78236f4f9e6b0876e476b78af0fbaff363160613
imsamuel1043 <samuelmorris1043@gmail.com>
2026-04-30 21:27:37 +0530

    update4

--- my-react-app/src/Components/Servicespage/Services.jsx
+++ my-react-app/src/Components/Servicespage/Services.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import servicesbg from "../../assets/Img/servicesbg.webp";
 import "../../assets/Css/Services.css";
 
-import aboutheroimg from "../../assets/Img/aboutherobg.jpg";
 import ServicesIntro from "./ServicesIntro";
 import ServicesGrid from "./ServicesGrid";
 import ServicesProcess from "./ServicesProcess";
@@ -10,41 +9,32 @@ import ServicesCTA from "./ServicesCTA";
 
 const Services = () => {
   return (
-
     <>
       <section
         className="services-hero"
         style={{ backgroundImage: `url(${servicesbg})` }}
       >
-
-        {/* Overlay */}
         <div className="services-overlay"></div>
 
         <div className="container">
-
           <div className="services-hero-content">
 
-            {/* Breadcrumb */}
             <p className="services-breadcrumb">
-              Home <span>|</span> About
+              Home <span>|</span> Services
             </p>
 
-            {/* Title */}
             <h2 className="services-hero-title">
               OUR SERVICES
             </h2>
 
           </div>
-
         </div>
-
       </section>
 
       <ServicesIntro />
       <ServicesGrid />
       <ServicesProcess />
       <ServicesCTA />
-
     </>
   );
 };
--- my-react-app/src/Components/Servicespage/ServicesGrid.jsx
+++ my-react-app/src/Components/Servicespage/ServicesGrid.jsx
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../../assets/Css/Services.css";
 
-// import img1 from "../../assets/Img/img1.jpg";
-// import img2 from "../../assets/Img/img2.jpg";
-// import img3 from "../../assets/Img/img3.jpg";
-// import img4 from "../../assets/Img/img4.jpg";
-// import img5 from "../../assets/Img/img5.jpg";
-
-// const images = [img1, img2, img3, img4, img5];
-
 const ServicesGrid = () => {
-  const [index, setIndex] = useState(0);
-
-  // Auto change images
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % images.length);
-    }, 2500);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <section className="services-hero1">
       <div className="container text-center">
@@ -32,21 +13,6 @@ const ServicesGrid = () => {
         <p className="subtitle">
           Designing serene, living spaces inspired by nature.
         </p>
-
-        {/*  */}
-
-        {/* <div className="image-stack">
-          {images.map((img, i) => (
-            <img
-              key={i}
-              src={img}
-              alt=""
-              className={`stack-img ${
-                i === index ? "active" : ""
-              }`}
-            />
-          ))}
-        </div> */}
 
       </div>
     </section>
